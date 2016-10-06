789ba0074a2983f6391bdc49c51eae120dfb6c10
Prateek Sharma <prateek13103718@gmail.com>
2016-10-06 17:47:52 +0530

    Project Type 2 upload

---
+++ Utility Type 2/WindowsFormsApplication4/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.title = new System.Windows.Forms.Label();
+            this.description = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.html = new System.Windows.Forms.Button();
+            this.excelToo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.title.Location = new System.Drawing.Point(247, 29);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(229, 29);
+            this.title.TabIndex = 0;
+            this.title.Text = "File Merger Utility";
+            // 
+            // description
+            // 
+            this.description.AutoSize = true;
+            this.description.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.description.Location = new System.Drawing.Point(168, 67);
+            this.description.Name = "description";
+            this.description.Size = new System.Drawing.Size(390, 20);
+            this.description.TabIndex = 1;
+            this.description.Text = "Merge html file tables, Convert them to excel easily";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(252, 106);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(196, 35);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Select File to Merge";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 17;
+            this.listBox1.Location = new System.Drawing.Point(61, 159);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(606, 497);
+            this.listBox1.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Fluor Daniel", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkCyan;
+            this.label1.Location = new System.Drawing.Point(670, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 19);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Help";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // html
+            // 
+            this.html.Location = new System.Drawing.Point(190, 680);
+            this.html.Name = "html";
+            this.html.Size = new System.Drawing.Size(99, 50);
+            this.html.TabIndex = 15;
+            this.html.Text = "Get .html only";
+            this.html.UseVisualStyleBackColor = true;
+            this.html.Click += new System.EventHandler(this.mergeButton_Click);
+            // 
+            // excelToo
+            // 
+            this.excelToo.Location = new System.Drawing.Point(414, 680);
+            this.excelToo.Name = "excelToo";
+            this.excelToo.Size = new System.Drawing.Size(160, 50);
+            this.excelToo.TabIndex = 16;
+            this.excelToo.Text = "Get both .html and .xlsx";
+            this.excelToo.UseVisualStyleBackColor = true;
+            this.excelToo.Click += new System.EventHandler(this.mergeButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(730, 777);
+            this.Controls.Add(this.excelToo);
+            this.Controls.Add(this.html);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.description);
+            this.Controls.Add(this.title);
+            this.Name = "Form1";
+            this.Text = "HTML FileMerger";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label description;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button html;
+        private System.Windows.Forms.Button excelToo;
+    }
+}
+
---
+++ Utility Type 2/WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.IO;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        bool typeOfOutput = false;
+        public static string msg ="Merged !!!!";
+        public static string HeaderFile = "";
+        public static List<string> fileNamesList = new List<string>();
+        //public static string[] fileNames = new string[60];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            {
+
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Multiselect = true;
+                dialog.Filter = "html files (*.html, *.htm)|*.html; *.htm;|All files (*.*)|*.*";
+                
+                dialog.Title = "Select the Validation file";
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    fileNamesList.Clear();
+                    listBox1.Items.Clear();
+                    
+                    HeaderFile = dialog.FileNames[0];
+                    listBox1.Items.Add(HeaderFile);
+                    for (int i = 1; i < dialog.FileNames.Length;i++ )
+                    {
+                        fileNamesList.Add(dialog.FileNames[i]);
+                        listBox1.Items.Add(dialog.FileNames[i]);
+                    }
+                    
+                }
+            }
+        }
+
+        private void mergeButton_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            
+            int numberOfFiles = fileNamesList.Count();
+
+            if (HeaderFile == "")
+            {
+                msg = "Nothing To do !!!";
+                MessageBox.Show(msg);
+            }
+            else if (numberOfFiles == 0)
+            {
+                msg = "File converted to Excel and stored at=> " + convert2Excel(HeaderFile);
+                MessageBox.Show(msg);
+            }
+
+            else
+            {
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+                if (b.Name == "html")
+                {
+                    saveFileDialog1.Filter = "HTML File|*.html; *.htm|All files (*.*)|*.*";
+                    saveFileDialog1.Title = "Save an HTML File";
+                }
+                else if (b.Name == "excelToo")
+                {
+                    saveFileDialog1.Filter = "All files (*.*)|*.*";
+                    saveFileDialog1.Title = "Save an HTML as well as Excel File";
+
+                }
+
+                // If the file name is not an empty string open it for saving.
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    if (saveFileDialog1.FileName != "")
+                    {
+                        string temp = Path.GetDirectoryName(saveFileDialog1.FileName) + "\\" + Path.GetFileNameWithoutExtension(saveFileDialog1.FileName) + ".html";
+                        string[] lines = File.ReadAllLines(HeaderFile);
+                        int totalLines = lines.Length;
+                        string[] appendLines = new string[8];
+                        for (int i = 1; i <= 8; i++)
+                        {
+                            appendLines[8 - i] = lines[totalLines - i];
+                            lines[totalLines - i] = "";
+                        }
+
+                        File.WriteAllLines(temp, lines);
+                        string[] arr;
+                        for (int l = 0; l < numberOfFiles; l++)
+                        {
+                            {
+                                arr = File.ReadAllLines(fileNamesList[l]);
+                                totalLines = arr.Length;
+                                for (int i = 0; i < 38; i++)
+                                    arr[i] = "";
+                                for (int i = 1; i <= 8; i++)
+                                {
+                                    arr[totalLines - i] = "";
+                                }
+                                File.AppendAllLines(temp, arr);
+                            }
+
+                        }
+                        File.AppendAllLines(temp, appendLines);
+                        msg = "File is merged and stored at => " + temp;
+                        if (b.Name == "excelToo")
+                            convert2Excel(temp);
+                    }
+                    else
+                        msg = "File name for saving was empty !!!";
+                    MessageBox.Show(msg);
+                }
+
+            }
+                        
+        }
+
+        private string convert2Excel(string tempFilePath)
+        {
+            string finalPath="";
+            var app = new Excel.Application();
+            try
+            {
+                var wb = app.Workbooks.Open(tempFilePath);
+                
+                finalPath = tempFilePath.Replace(".html", ".xlsx");
+                
+                wb.SaveAs(finalPath, Excel.XlFileFormat.xlOpenXMLWorkbook);
+
+                wb.Close();
+                msg = "File is merged and stored at => " + finalPath;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                app.Quit();
+                
+            }
+            return finalPath;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This is a utility to merge different html files and output html or excel. If only one file selected,it will output only an excel file. But if more are selected, it is upto you if you want .html only or both .html or .xlsx !!!!!");
+        }
+
+    }
+}
